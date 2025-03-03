986fe19084ae62eea4fe3570615c7e65aaa30f87
Liz <lizmarie.mendez86@gmail.com>
2025-03-03 17:52:30 -0500

    First Day, fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+// import Home from "./pages/Home";
+// import Launches from "./pages/LaunchList";
+// import './styles/App.css';
+
+// - Welcome Page with how to search for launches
+// - Search bar to filter launches.
+//     - We want to search by two locations: Vandy and Capes to display five launches
+//launchpad  https://api.spacexdata.com/v4/launchpads/5e9e4502f509094188566f88
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fetchLaunches, setFetchLaunches] = useState([]);
+  useEffect(()=>{
+    fetch('https://api.spacexdata.com/v5/launches/latest')
+      .then(res => res.json())
+      .then(data => {
+        setFetchLaunches(data)
+        console.log(data.launchpad)
+          // console.log(data.launchpad.name)
+      })
+      .catch(error => console.error('Error fetching data:', error)) //handles errors
+ 
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>SpaceXplorer</h1>
+      <h2>Search by location: Vanderberg AFB or Cape Canaveral</h2>
     </>
   )
 }
