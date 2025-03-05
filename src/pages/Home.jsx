77724baaf82c9ddaad76c8fcc31ec958639879e8
harman1gidda <giddaharman@gmail.com>
2025-03-05 10:05:06 -0800

    working MVP code without GoBack button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         <div className='HomePageContainer'>
             <div id='WelcomeMessage'>
                 <h1>Welcome to SpaceXplorer</h1></div>
-            <p>SpaceXplorer is a web application that allows you to explore the world of SpaceX. <br></br>You can view information about SpaceX's rockets, capsules, and crew members. You can also view previous live streams of launches. SpaceXplorer is the ultimate destination for all things SpaceX!</p>
+            <div id='SubMessage'><p>SpaceXplorer is a web application that allows you to explore the world of SpaceX. <br></br>You can view information about SpaceX's rockets, capsules, and crew members. You can also view previous live streams of launches. SpaceXplorer is the ultimate destination for all things SpaceX!</p></div>
             {/* <img src='' alt=''></img>
             <img src='' alt=''></img>
             <img src='' alt=''></img> */}
